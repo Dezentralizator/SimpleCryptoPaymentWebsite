1c2017e01039eb064fa433008e5d5c5bb411c176
admin <admin@admins-MacBook-Pro.local>
2022-07-14 21:35:49 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,41 @@
+import logo from '../../images/logo.png';
+
+const Footer = () => {
+    return(
+        <div className='flex w-full md:justify-center justify-between flex-col items-center p-4 gradient-bg-footer '>
+            <div className='w-full flex sm:flex-row flex-col justify-between items-center my-4'>
+                <div className='flex flex-[0.5] justify-center items-center'>
+                    <img 
+                    src={logo}
+                    alt="logo"
+                    className='w-32'/>
+                </div>
+                <div className='flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full'>
+                    <p className='text-white text-base text-center mx-2 cursor-pointer'>Market</p>
+                    <p className='text-white text-base text-center mx-2 cursor-pointer'>Exchange</p>
+                    <p className='text-white text-base text-center mx-2 cursor-pointer'>Tutorials</p>
+                    <p className='text-white text-base text-center mx-2 cursor-pointer'>Wallets</p>
+                </div>
+            </div>
+            <div className='flex flex-col justify-center items-center mt-5'>
+                <p className='text-white test-sm text-center'>
+                    Come Join Us
+                </p>
+                <p className='text-white test-sm text-center'>
+                    My address
+                </p>
+            </div>
+            <div className='sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5'></div>
+            <div className='sm:w-[90%] w-full flex justify-between items-center mt-3'>
+            <p className='text-white test-sm text-center'>
+                    My Company
+                </p>
+                <p className='text-white test-sm text-center'>
+                    All rights reserved
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer; 
---
+++ src/utils/constants.js
@@ -0,0 +1,6 @@
+import abi from './Transactions.json';
+
+
+export const contractAddress = '0x6FFfA7e9B91CdF8E6745543cE348E0B644Bc9B38';
+export const contractABI = abi.abi;
+
